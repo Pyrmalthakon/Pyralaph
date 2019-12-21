d5b68c6233883c42718ce1241fd2d342014fd5f5
hrt <qirimhangli@gmail.com>
2019-12-21 14:30:14 +0100

    .

--- kernel.c
+++ kernel.c
@@ -1,14 +1,13 @@
 void main(void) //fonction chargée en premier par boot.S
 {
-    ktab("BOOTING......", '0');
+    int runshell = 1;
     free_vram();
     nextpage();
     
-    ktab("BOOTED!\n", 0x03);
-    ktab("Aferfer OS, v0.3\n\n\n", 0x03);
+    ktab("BOOT\n", 0x03);
+    ktab("Anzar OS", 0x03);
    
-
-    while(1)
+    while(runshell)
     {
 	shell();
     }
